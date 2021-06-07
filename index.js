34297a1b06351b0224d57d49a1e02155a720c8b0
Divyanshu Kumar <71032839+Diva-upa@users.noreply.github.com>
2021-06-07 09:51:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Apps from "./components/Apps";
+
+ReactDOM.render(
+  <div>
+    <Apps />
+  </div>,
+  document.getElementById("root")
+);
